494c3e2b883b45feec7c9ea06bf529a033fa9ec4
Felix Navrátil <felix.navratil@student.gyarab.cz>
2024-05-30 13:47:28 +0200

    commit 30.5

--- filmy/admin.py
+++ filmy/admin.py
@@ -2,18 +2,33 @@ from django.contrib import admin
 from filmy.models import Movie, Director, Genre, Actor
 
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "year", "footage", "director", "genres_display"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name", "director__name"]
+    list_filter = ["year", "genres"]
+    
 
 
 class DirectorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "birth_year"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name"]
+    list_filter = ["birth_year"]
+    
 
 
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name"]
+
 
 class ActorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "birth_year"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name"]
+    list_filter = ["birth_year"]
+
 
 admin.site.register(Movie, MovieAdmin)
 
